385d3f8eb011c90ef3ed5adff6fb3be7f271420a
likcoras <lck.ogc@gmail.com>
2015-06-19 21:49:18 -0600

    Remember to add notice

--- src/main/java/io/github/likcoras/agario/Handler.java
+++ src/main/java/io/github/likcoras/agario/Handler.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2015 likcoras
+ * 
+ * This file is part of A-Gario
+ * 
+ * A-Gario is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * A-Gario is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with A-Gario.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.github.likcoras.agario;
 
 import org.pircbotx.Channel;
