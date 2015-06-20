e8fff02c8699ab9630e4ce631d519f848c352739
likcoras <lck.ogc@gmail.com>
2015-06-20 09:37:58 -0600

    Simplify regex

--- src/main/java/io/github/likcoras/agario/WebsiteHandler.java
+++ src/main/java/io/github/likcoras/agario/WebsiteHandler.java
@@ -34,7 +34,7 @@ public class WebsiteHandler implements Handler {
 	private static final Logger LOG = Logger.getLogger(WebsiteHandler.class);
 	
 	private static final Pattern LINK_PATTERN = Pattern
-		.compile("(?i)\\b(http(s)?://([a-z0-9-]+\\.)+[a-z0-9]+(/\\S*)?)\\b");
+		.compile("(?i)\\b(https?://([a-z0-9-]+\\.)+[a-z0-9]+(/\\S*)?)\\b");
 	
 	@Override
 	public void configure(BotConfig config) {}
