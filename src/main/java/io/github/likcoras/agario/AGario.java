6ab245614226b79a86a9b84a726cc112117e44e0
likcoras <lck.ogc@gmail.com>
2015-06-23 22:37:21 -0600

    Re-add WebsiteHandler

--- src/main/java/io/github/likcoras/agario/AGario.java
+++ src/main/java/io/github/likcoras/agario/AGario.java
@@ -134,7 +134,7 @@ public class AGario extends ListenerAdapter<PircBotX> {
 	private List<Handler> getHandlers() {
 		return ImmutableList.<Handler> builder().add(new LinkHandler())
 			.add(new InfoHandler()).add(new ServersHandler())
-			.add(new IsonHandler()).build();
+			.add(new WebsiteHandler()).add(new IsonHandler()).build();
 	}
 	
 	private void configure(List<Handler> handlers, BotConfig config)
---
+++ src/main/java/io/github/likcoras/agario/WebsiteHandler.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2015 likcoras
+ * 
+ * This file is part of A-Gario
+ * 
+ * A-Gario is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * A-Gario is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with A-Gario.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.likcoras.agario;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.pircbotx.Channel;
+import org.pircbotx.PircBotX;
+import org.pircbotx.User;
+import org.pircbotx.hooks.Event;
+
+public class WebsiteHandler implements Handler {
+	
+	private static final Logger LOG = Logger.getLogger(WebsiteHandler.class);
+	
+	private static final Pattern LINK_PATTERN = Pattern
+		.compile("(?i)\\b(https?://(\\S*)");
+	
+	@Override
+	public void configure(BotConfig config) {}
+	
+	@Override
+	public boolean handlesEvent(Event<PircBotX> event) {
+		return false;
+	}
+	
+	@Override
+	public void handleEvent(Event<PircBotX> event) {}
+	
+	@Override
+	public boolean isHandlerOf(Channel chan, User user, String message) {
+		return LINK_PATTERN.matcher(message).find();
+	}
+	
+	@Override
+	public String getResponse(Channel chan, User user, String message) {
+		final Matcher match = LINK_PATTERN.matcher(message);
+		match.find();
+		final String url = match.group(1);
+		LOG.info("Title for " + url + " requested");
+		try {
+			String title = Jsoup.connect(url).userAgent("").get().title();
+			if (!title.isEmpty())
+				return user.getNick() + ": " + title;
+		} catch (final IOException e) {}
+		return "";
+	}
+	
+}
