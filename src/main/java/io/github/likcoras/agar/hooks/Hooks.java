15986d197d519419cb64a63882afa6249d68c7ac
likcoras <lck.ogc@gmail.com>
2015-10-17 20:10:57 -0600

    Added hooks

---
+++ src/main/java/io/github/likcoras/agar/hooks/Hooks.java
@@ -0,0 +1,16 @@
+package io.github.likcoras.agar.hooks;
+
+import io.github.likcoras.agar.AgarBot;
+
+import com.google.common.collect.ImmutableList;
+import org.pircbotx.hooks.Listener;
+
+import java.util.List;
+
+public class Hooks {
+    public static final List<Listener<AgarBot>> LIST =
+            ImmutableList.builder().add(new CoreHook())
+                    .add(new QuitHook()).add(new RawHook()).add(new AuthHook())
+                    .add(new IgnoreHook()).add(new LinkHook())
+                    .add(new BadwordHook()).build();
+}
