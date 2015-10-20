282de8c2b48c4234c3bb392451dd8127638cf0d5
likcoras <lck.ogc@gmail.com>
2015-10-20 10:49:59 -0600

    Add bypass level to bypass spam checking

--- src/main/java/io/github/likcoras/agar/Spam.java
+++ src/main/java/io/github/likcoras/agar/Spam.java
@@ -1,5 +1,7 @@
 package io.github.likcoras.agar;
 
+import io.github.likcoras.agar.auth.AuthLevel;
+
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import org.pircbotx.User;
@@ -12,11 +14,19 @@ public class Spam {
             .expireAfterWrite(10L, TimeUnit.SECONDS).build();
             
     public boolean check(User user) {
+        if (checkBypass(user)) {
+            return true;
+        }
         UUID uid = user.getUserId();
         if (spam.getIfPresent(uid) != null) {
             return false;
         }
         spam.put(uid, true);
         return true;
+    }
+    
+    private boolean checkBypass(User user) {
+        AgarBot bot = (AgarBot) user.getBot();
+        return bot.getAuth().checkLevel(user, AuthLevel.BYPASS);
     }
 }
--- src/main/java/io/github/likcoras/agar/auth/AuthLevel.java
+++ src/main/java/io/github/likcoras/agar/auth/AuthLevel.java
@@ -1,5 +1,5 @@
 package io.github.likcoras.agar.auth;
 
 public enum AuthLevel {
-    USER, MOD, ADMIN, OWNER;
+    USER, BYPASS, MOD, ADMIN, OWNER;
 }
