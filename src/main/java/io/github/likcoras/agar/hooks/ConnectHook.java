4a5034c7b041ec01a0f1741cc3e792a8bba5f0b5
likcoras <lck.ogc@gmail.com>
2015-10-20 12:39:14 -0600

    Minor formatting

--- src/main/java/io/github/likcoras/agar/hooks/ConnectHook.java
+++ src/main/java/io/github/likcoras/agar/hooks/ConnectHook.java
@@ -11,16 +11,22 @@ import org.pircbotx.hooks.types.GenericMessageEvent;
 import java.io.IOException;
 
 public class ConnectHook extends ListenerAdapter<AgarBot> {
-    private static final String API_DATA = "http://connect.agariomods.com/json/api.php?action=GSFUC&username=";
-    private static final String NO_USER = Utils.addFormat("&04No user named &r");
-    private static final String NO_IMG = Utils.addFormat("&04No image uploaded by &r");
-    private static final String INFO = Utils.addFormat("&03User: &r%s | &03Skin: &rhttps://connect.agariomods.com/img_%s.png");
-    
+    private static final String API_DATA =
+            "http://connect.agariomods.com/json/api.php?action=GSFUC&username=";
+    private static final String NO_USER =
+            Utils.addFormat("&04No user named &r");
+    private static final String NO_IMG =
+            Utils.addFormat("&04No image uploaded by &r");
+    private static final String INFO = Utils.addFormat(
+            "&03User: &r%s | &03Skin: &rhttps://connect.agariomods.com/img_%s.png");
+            
     @Override
-    public void onGenericMessage(GenericMessageEvent<AgarBot> event) throws IOException {
+    public void onGenericMessage(GenericMessageEvent<AgarBot> event)
+            throws IOException {
         AgarBot bot = event.getBot();
         String message = event.getMessage();
-        if (!Utils.isTrigger(message, "@con ") || !bot.getSpam().check(event.getUser())) {
+        if (!Utils.isTrigger(message, "@con ")
+                || !bot.getSpam().check(event.getUser())) {
             return;
         }
         String username = message.substring(5).toLowerCase();
@@ -41,7 +47,6 @@ public class ConnectHook extends ListenerAdapter<AgarBot> {
     @Value
     private class ConnectInfo {
         String username;
-        @SerializedName(value = "has_img")
-        boolean hasImg;
+        @SerializedName(value = "has_img") boolean hasImg;
     }
 }
